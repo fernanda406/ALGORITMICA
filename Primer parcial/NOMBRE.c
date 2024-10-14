75ffbcb93c39fdab38de799db69ebe3f53c27943
fernanda406 <24020132@uadeo.mx>
2024-10-14 13:58:43 -0700

    Add files via upload

---
+++ Primer parcial/NOMBRE.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+ 
+ int main()
+   {
+     
+     char nombre[20];
+
+     printf("Introduce tu Nombre: ");
+     scanf("%s", nombre );
+     printf("Introduce tu Apellido: ");
+     scanf("%s", apellido );
+     printf("Hola %s %s, buenos d%cas. ", nombre, apellido, 161);
+
+     getch();
+     return 0;
+   }
