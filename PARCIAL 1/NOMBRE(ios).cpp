99e242219117d38ba4bed60ad35606eb059b3824
fernanda406 <24020132@uadeo.mx>
2024-11-20 15:19:42 -0700

    Add files via upload

---
+++ PARCIAL 1/NOMBRE(ios).cpp
@@ -0,0 +1,20 @@
+//NOMBRE
+#include <iostream>
+
+ using namespace std;
+
+ int main()
+  {
+    char nombre[20], apellido[20];
+
+    cout<< "Cual es tu Nombre: " ;
+    cin>> nombre ;
+
+    cout<< "Cual es tu apellido: " ;
+    cin>> apellido ;
+
+    cout<<"Hola" << nombre << apellido <<
+
+    return 0;
+
+  }
